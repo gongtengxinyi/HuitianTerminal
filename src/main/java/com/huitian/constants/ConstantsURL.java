174fb428cd047fd9efbbc85e5926841d55b505c1
dingjianlei <1251272104@qq.com>
2017-09-25 10:21:34 +0800

    初始化测试

--- src/main/java/com/huitian/constants/ConstantsURL.java
+++ src/main/java/com/huitian/constants/ConstantsURL.java
@@ -8,7 +8,7 @@ public class ConstantsURL {
     /**
      * 登陆获取接口的url地址
      */
-    public static final String URL = "http://localhost:7080/wapi/huitianterminal/login";
+    public static final String URL = "http://localhost:9999/wapi/huitianterminal/login";
     /**
      * 测试网络状态 此处先用百度 ，以后改用wapi接口测试
      */
@@ -16,5 +16,5 @@ public class ConstantsURL {
     /**
      * 远程rest风格接口列表
      */
-    public  static  final  String WEBSOCKET_ADDRESS="ws://localhost:8080/chat/chatServer";
+    public  static  final  String WEBSOCKET_ADDRESS="ws://localhost:8888/chat/chatServer";
 }
